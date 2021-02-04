3720114215726deeca1e551d2f0fb14230b55738
chenshijie <chenshijie@uniontech.com>
2021-02-04 16:42:43 +0800

    feat: 添加打印信息
    description: 添加打印信息
    
    Log: 添加打印信息
    Change-Id: I9fe6823b6e6522e5315b8bffbe5f88860bfe998d

--- src/module/implugin/implugin-service.c
+++ src/module/implugin/implugin-service.c
@@ -1,25 +1,25 @@
 /*
-* Copyright (C) 2021 ~ 2025 Uniontech Software Technology Co.,Ltd.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2021 ~ 2025 Uniontech Software Technology Co.,Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/inotify.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 #include "fcitx-utils/utils.h"
@@ -43,14 +43,14 @@ char *find_target(const char *inputFIleInfo) {
         return NULL;
     }
     //[1] 第一次与 "fcitx-" 不匹配的字段位置
-    const char * sPointPosition = strstr(inputFIleInfo, "fcitx-");
-    if(sPointPosition == NULL){
+    const char *sPointPosition = strstr(inputFIleInfo, "fcitx-");
+    if (sPointPosition == NULL) {
         return NULL;
     }
-    int startPosition = (sPointPosition - inputFIleInfo ) + strlen("fcitx-");
+    int startPosition = (sPointPosition - inputFIleInfo) + strlen("fcitx-");
     //[2] 第一次与 ".so" 匹配的字段位置
     const char *ePointPosition = strstr(inputFIleInfo, ".so");
-    if(ePointPosition == NULL){
+    if (ePointPosition == NULL) {
         return NULL;
     }
     size_t endPosition = ePointPosition - inputFIleInfo;
@@ -69,30 +69,28 @@ char *find_target(const char *inputFIleInfo) {
     return NULL;
 }
 
-void delete_comment(char str[]){
-    int i,j;
-    for(i=j=0;str[i]!='\0';i++){
-        if(str[i]!='\#' || str[i+1]==' ')
-        {
-            str[j++]=str[i];
+void delete_comment(char str[]) {
+    int i, j;
+    for (i = j = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\#' || str[i + 1] == ' ') {
+            str[j++] = str[i];
         }
     }
-    str[j]='\0';
+    str[j] = '\0';
 }
 
-void file_checkout(const char * filename) {
-    int fd,len;
+void file_comment_checkout(const char *filename) {
+    int fd, len;
     char str[BUFSIZ];
 
-    fd=open(filename,O_CREAT|O_RDWR,S_IRUSR|S_IWUSR);
-    if(fd){
-    len=read(fd,str,BUFSIZ);
-    str[len]='\0';
-    delete_comment(str);
-    ftruncate(fd, 0);
-    lseek(fd, 0, SEEK_SET);
-    write(fd,str,strlen(str));
-
+    fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd) {
+        len = read(fd, str, BUFSIZ);
+        str[len] = '\0';
+        delete_comment(str);
+        ftruncate(fd, 0);
+        lseek(fd, 0, SEEK_SET);
+        write(fd, str, strlen(str));
     }
     close(fd);
 }
@@ -101,17 +99,13 @@ void file_checkout(const char * filename) {
 
 char *event_str[EVENT_NUM] = {"IN_ACCESS",
                               "IN_MODIFY", //文件修改
-                              "IN_ATTRIB",
-                              "IN_CLOSE_WRITE",
-                              "IN_CLOSE_NOWRITE",
-                              "IN_OPEN",
+                              "IN_ATTRIB",        "IN_CLOSE_WRITE",
+                              "IN_CLOSE_NOWRITE", "IN_OPEN",
                               "IN_MOVED_FROM", //文件移动from
                               "IN_MOVED_TO",   //文件移动to
                               "IN_CREATE",     //文件创建
                               "IN_DELETE",     //文件删除
-                              "IN_DELETE_SELF",
-                              "IN_MOVE_SELF"};
-
+                              "IN_DELETE_SELF",   "IN_MOVE_SELF"};
 
 int main(int argc, char *argv[]) {
 
@@ -132,16 +126,18 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     fp = FcitxXDGGetFileUserWithPrefix("conf", "fcitx-defaultim.config", "r",
                                        &dimConfigPath);
-    if (fp)
+    if (fp) {
         fclose(fp);
-    file_checkout(dimConfigPath);
+        file_comment_checkout(dimConfigPath);
+    }
 
     //获取输入法插件配置文件路径
     fp = FcitxXDGGetFileUserWithPrefix("conf", "fcitx-implugin.config", "r",
                                        &imPluginConfigPath);
-    if (fp)
+    if (fp) {
         fclose(fp);
-    file_checkout(imPluginConfigPath);
+        file_comment_checkout(imPluginConfigPath);
+    }
 
     if (!fcitxLibPath)
         return -1;
@@ -157,8 +153,9 @@ int main(int argc, char *argv[]) {
      * 监听所有事件：IN_ALL_EVENTS
      * 监听文件是否被创建,删除,移动：IN_CREATE|IN_DELETE|IN_MOVED_FROM|IN_MOVED_TO
      */
-    inotifyWd = inotify_add_watch(inotifyFd, fcitxLibPath,
-                           IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
+    inotifyWd =
+        inotify_add_watch(inotifyFd, fcitxLibPath,
+                          IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
     if (inotifyWd < 0) {
         fprintf(stderr, "inotify_add_watch %s failed\n", argv[1]);
         return -1;
@@ -174,7 +171,7 @@ int main(int argc, char *argv[]) {
                     if (event->len > 0) {
                         //获取输入法名称
                         char *imName = find_target(event->name);
-                        if(imName == NULL){
+                        if (imName == NULL) {
                             continue;
                         }
                         if (fcitx_utils_strcmp0(event_str[i], "IN_CREATE") ==
@@ -203,15 +200,13 @@ int main(int argc, char *argv[]) {
                                     (strlen(settingWizard) + 1) * sizeof(char));
 
                             char parameter[BUFSIZ];
-                            memset(parameter, 0,
-                                   FCITX_ARRAY_SIZE(parameter));
-                            ini_gets(imName, "Parameter", NULL,
-                                     parameter,
+                            memset(parameter, 0, FCITX_ARRAY_SIZE(parameter));
+                            ini_gets(imName, "Parameter", NULL, parameter,
                                      FCITX_ARRAY_SIZE(parameter),
                                      imPluginConfigPath);
 
-                            char *pParameter = malloc(
-                                (strlen(parameter) + 1) * sizeof(char));
+                            char *pParameter =
+                                malloc((strlen(parameter) + 1) * sizeof(char));
                             memset(pParameter, 0,
                                    (strlen(parameter) + 1) * sizeof(char));
                             strncpy(pParameter, parameter,
@@ -220,22 +215,15 @@ int main(int argc, char *argv[]) {
                             if (fcitx_utils_strcmp0(pSettingWizard, "none") !=
                                 0) {
 
-                                if(pParameter){
-                                    char* commod[] = {
+                                if (pParameter) {
+                                    char *commod[] = {
                                         pSettingWizard,
-                                        (char*)(intptr_t)pParameter,
-                                        NULL
-                                    };
+                                        (char *)(intptr_t)pParameter, NULL};
+                                    fcitx_utils_start_process(commod);
+                                } else {
+                                    char *commod[] = {pSettingWizard, NULL};
                                     fcitx_utils_start_process(commod);
                                 }
-                                else {
-                                    char* commod[] = {
-                                        pSettingWizard,
-                                        NULL
-                                    };
-                                    fcitx_utils_start_process(commod);
-                                }
-
                             }
                             free(pSettingWizard);
                             free(pParameter);
@@ -245,9 +233,8 @@ int main(int argc, char *argv[]) {
                             char curDeimName[BUFSIZ];
                             memset(curDeimName, 0,
                                    FCITX_ARRAY_SIZE(curDeimName));
-                            ini_gets("DefaultIM", "IMNAME",
-                                     "fcitx-keyboard-us", curDeimName,
-                                     FCITX_ARRAY_SIZE(curDeimName),
+                            ini_gets("DefaultIM", "IMNAME", "fcitx-keyboard-us",
+                                     curDeimName, FCITX_ARRAY_SIZE(curDeimName),
                                      dimConfigPath);
 
                             char *pCurDeimName = malloc(
@@ -260,8 +247,7 @@ int main(int argc, char *argv[]) {
                             if (fcitx_utils_strcmp0(pCurDeimName, imName) ==
                                 0) {
                                 ini_puts("DefaultIM", "IMNAME",
-                                         "fcitx-keyboard-us",
-                                         dimConfigPath);
+                                         "fcitx-keyboard-us", dimConfigPath);
                             }
 
                             free(pCurDeimName);
