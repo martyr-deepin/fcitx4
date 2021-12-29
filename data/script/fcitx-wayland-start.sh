850bd674acff4cc49a7cc08c67e6712f0c69da9e
zhaoyue <zhaoyue@uniontech.com>
2021-12-29 11:24:30 +0800

    fix: 修改wayland下输入法启动异常的bug
    修改启动脚本，wayland平台下等dbus服务启动成功后在启动fcitx
    
    Log:
    
    Bug: https://pms.uniontech.com/zentao/bug-view-97024.html
    Change-Id: I16443fa872784c8037b8fac67289ea020576d253

---
+++ data/script/fcitx-wayland-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 脚本解释器 强制设置为 bash
+if [ "$BASH" != "/bin/bash" ] && [ "$BASH" != "/usr/bin/bash" ]; then
+    bash "$0" "$@"
+    exit $?
+fi
+
+var1=`systemctl status dbus.service`
+var2="active (running)"
+result=$(echo $var1 | grep "${var2}")
+while [ "$result" == "" ]
+do
+        sleep 2
+done
